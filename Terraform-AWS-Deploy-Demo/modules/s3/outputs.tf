8ef3af5c931d882492c0bd29872050dedd4b5aba
Damien Cooke <damienncooke@gmail.com>
2026-07-02 17:53:00 -0400

    Changed output variable name using "s3" to "bucket"

--- Terraform-AWS-Deploy-Demo/modules/s3/outputs.tf
+++ Terraform-AWS-Deploy-Demo/modules/s3/outputs.tf
@@ -1,14 +1,14 @@
-output "s3_id" {
+output "bucket_id" {
   description = "Name of bucket"
   value = aws_s3_bucket.applogs.id
 }
 
-output "s3_arn" {
+output "bucket_arn" {
   description = "ARN of bucket"
   value = aws_s3_bucket.applogs.arn
 }
 
-output "s3_domain" {
+output "bucket_domain" {
   description = "Bucket domain name"
   value = aws_s3_bucket.applogs.bucket_domain_name
 }
