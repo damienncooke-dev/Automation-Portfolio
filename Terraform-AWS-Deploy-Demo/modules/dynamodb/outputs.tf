2c83555d1eea89b7ed4d9aa15fa28d30964114f5
Damien Cooke <damienncooke@gmail.com>
2026-07-03 10:14:20 -0400

    - Modified variable name "tenant_namespace" to "market_namespace" - Corrected typo in outputs.tf, changed "dynamo" to "dynamodb"

--- Terraform-AWS-Deploy-Demo/modules/dynamodb/outputs.tf
+++ Terraform-AWS-Deploy-Demo/modules/dynamodb/outputs.tf
@@ -1,10 +1,10 @@
 output "table_id" {
   description = "Name of table"
-  value = aws_dynamo_table.log_index.id
+  value = aws_dynamodb_table.log_index.id
 }
 
 output "table_arn" {
   description = "ARN of table"
-  value = aws_dynamo_table.log_index.arn
+  value = aws_dynamodb_table.log_index.arn
 }
 
--- Terraform-AWS-Deploy-Demo/modules/dynamodb/variables.tf
+++ Terraform-AWS-Deploy-Demo/modules/dynamodb/variables.tf
@@ -8,3 +8,4 @@ variable "market_namespace" {
   type = string
   default = "market-a"
 }
+
