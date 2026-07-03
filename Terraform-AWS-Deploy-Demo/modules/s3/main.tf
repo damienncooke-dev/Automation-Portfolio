1c9f32091dc7028b1d54facf7f50167f7c3fa56e
Damien Cooke <damienncooke@gmail.com>
2026-07-03 10:06:47 -0400

    - Modified output names in output.tf to "bucket_arn" - Modified 'applogs' to 'app_logs' in main.tf

--- Terraform-AWS-Deploy-Demo/modules/s3/main.tf
+++ Terraform-AWS-Deploy-Demo/modules/s3/main.tf
@@ -1,3 +1,3 @@
-resource "aws_s3_bucket" "applogs" {
+resource "aws_s3_bucket" "app_logs" {
   bucket = "demo-${var.environment}-${var.market_namespace}-logs"
 }
--- Terraform-AWS-Deploy-Demo/modules/s3/outputs.tf
+++ Terraform-AWS-Deploy-Demo/modules/s3/outputs.tf
@@ -1,14 +1,9 @@
 output "bucket_id" {
   description = "Name of bucket"
-  value = aws_s3_bucket.applogs.id
+  value = aws_s3_bucket.app_logs.id
 }
 
 output "bucket_arn" {
   description = "ARN of bucket"
-  value = aws_s3_bucket.applogs.arn
-}
-
-output "bucket_domain" {
-  description = "Bucket domain name"
-  value = aws_s3_bucket.applogs.bucket_domain_name
+  value = aws_s3_bucket.app_logs.arn
 }
