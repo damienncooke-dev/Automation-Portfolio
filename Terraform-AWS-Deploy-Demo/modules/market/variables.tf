99e2d8f859309b834a90f7f570e80f5e162dc749
Damien Cooke <damienncooke@gmail.com>
2026-07-07 12:23:26 -0400

    - Renamed "market_namespace" to "market_region" - Moved line "description" - Removed default value, not needed.

--- Terraform-AWS-Deploy-Demo/modules/market/variables.tf
+++ Terraform-AWS-Deploy-Demo/modules/market/variables.tf
@@ -12,3 +12,18 @@ variable "trusted_role_arn" {
   type = string
   description = "Used to grant the user permission to assume role"
 }
+
+variable "billing_mode" {
+  type    = string
+}
+
+# This ami_id is specific to "us-east-1/N.Virginia"
+variable "ami_id" {
+  type    = string
+}
+
+variable "force_destroy" {
+  type    = bool
+}
+
+
