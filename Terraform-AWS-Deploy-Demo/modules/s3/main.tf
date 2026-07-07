654aeb82eb08e2aed2d7f90bc57246d3ff9af0aa
Damien Cooke <damienncooke@gmail.com>
2026-07-07 12:15:04 -0400

    - Changed "var.market_namespace" to "var.market_region" in bucket name - Added "force_destroy" option - Added server side encryption - Added block all public access

--- Terraform-AWS-Deploy-Demo/modules/s3/main.tf
+++ Terraform-AWS-Deploy-Demo/modules/s3/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "app_logs" {
   bucket = "demo-${var.environment}-${var.market_region}-logs"
-  force_destroy = var.delete_data
+  force_destroy = var.force_destroy
 }
 
 # DevSecOps Best Practice: Ensure the state bucket is fully encrypted
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "prod_data_crypto"
 
 # DevSecOps Best Practice: Block all public access to your state file
 resource "aws_s3_bucket_public_access_block" "prod_data_privacy" {
-  bucket                  = aws_s3_bucket.terraform_state.id
+  bucket                  = aws_s3_bucket.app_logs.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
