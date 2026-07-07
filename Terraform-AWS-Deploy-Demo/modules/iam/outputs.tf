79afd6a9ea7fae6092dee11b2b8ac059535aa1cf
Damien Cooke <damienncooke@gmail.com>
2026-07-07 13:13:58 -0400

    - Added "instance_profile_name" to be exposed to calling root module /markets/main.tf

--- Terraform-AWS-Deploy-Demo/modules/iam/outputs.tf
+++ Terraform-AWS-Deploy-Demo/modules/iam/outputs.tf
@@ -8,4 +8,12 @@ output "role_arn" {
   value = aws_iam_role.market_role.arn
 }
 
+output "market_instance" {
+  description = "The instance profile to attach to ec2 instance"
+  value = aws_iam_instance_profile.ec2_profile.name
+}
 
+output "instance_profile_name" {
+  description = "The instance profile to attach to ec2 instance"
+  value = aws_iam_instance_profile.ec2_profile.name
+}
