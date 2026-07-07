91dc044586f6829ae0c3ebdc68e54e4337486516
Damien Cooke <damienncooke@gmail.com>
2026-07-07 13:29:33 -0400

    - Renamed "market_namespace" to "market_region" - Added variable "billing_mode"

--- Terraform-AWS-Deploy-Demo/environments/prod/variables.tf
+++ Terraform-AWS-Deploy-Demo/environments/prod/variables.tf
@@ -21,6 +21,18 @@ variable "trusted_role_arn" {
 }
 
 variable "billing_mode" {
-  type        = string
-  default = "PROVISIONED"
+  type    = string
+  default = "PAY_PER_REQUEST"
 }
+
+# This ami_id is specific to "us-west-1/N.California"
+variable "ami_id" {
+  type    = string
+  default = "ami-07fdf51168766b58a"
+}
+
+variable "force_destroy" {
+  type    = bool
+  default = true
+}
+
