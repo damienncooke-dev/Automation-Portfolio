f4b10d9e2d7a8b805cf031b1f5fb20ffdac6cce3
Damien Cooke <damienncooke@gmail.com>
2026-07-09 22:22:41 -0400

    - Updated comments under module/ec2 and module/iam - Executed terraform format

--- Terraform-AWS-Deploy-Demo/modules/market/main.tf
+++ Terraform-AWS-Deploy-Demo/modules/market/main.tf
@@ -1,30 +1,30 @@
 module "s3" {
-  source           = "../s3"
-  environment      = var.environment
-  market_region    = var.market_region
-  force_destroy    = var.force_destroy
+  source        = "../s3"
+  environment   = var.environment
+  market_region = var.market_region
+  force_destroy = var.force_destroy
 }
 
 module "dynamodb" {
-  source           = "../dynamodb"
-  environment      = var.environment
-  market_region    = var.market_region
-  billing_mode     = var.billing_mode
+  source        = "../dynamodb"
+  environment   = var.environment
+  market_region = var.market_region
+  billing_mode  = var.billing_mode
 }
 
 module "ec2" {
-  source           = "../ec2"
-  environment      = var.environment
-  market_region    = var.market_region
-  ami_id           = var.ami_id   # comes directly from the root module in either dev or prod
-  iam_instance_profile   = module.iam.instance_profile_name   # pulls the instance profile from the iam output "instance_profile"
+  source               = "../ec2"
+  environment          = var.environment
+  market_region        = var.market_region
+  ami_id               = var.ami_id                       # Passed through from calling 'root' module. Specifies OS to be used in EC2
+  iam_instance_profile = module.iam.instance_profile_name # Gets value from the 'output' of the iam module
 }
 
 module "iam" {
   source           = "../iam"
   environment      = var.environment
   market_region    = var.market_region
-  bucket_arn       = module.s3.bucket_arn      # this variable must appear in iam/variables.tf. you will get "unsupported argument" if the module doesn't expect this input.
-  table_arn        = module.dynamodb.table_arn #  ^(same)^
-  trusted_role_arn = var.trusted_role_arn      #  ^(same)^
+  bucket_arn       = module.s3.bucket_arn      # Gets value from the 'output' of the S3 module
+  table_arn        = module.dynamodb.table_arn # Gets value from the 'output' of the dynamodb module
+  trusted_role_arn = var.trusted_role_arn      # Passed through from calling 'root' module
 }
